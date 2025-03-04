06abd44aa919cb2c4dc490282f834f2d6a467591
HR <hrcode275@gmail.com>
2025-03-04 14:03:40 -0600

    Sepolia y mainnet

--- decentralized-exchange/hardhat.config.js
+++ decentralized-exchange/hardhat.config.js
@@ -1,15 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
+
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    rinkeby: {
-      url: "https://rinkeby.infura.io/v3/YOUR_INFURA_PROJECT_ID",
-      accounts: ["YOUR_PRIVATE_KEY"]
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/f158361d906a45c49c91c8b6cd197c2f",
+      accounts: ["TU_LLAVE_PRIVADA"]
     },
     mainnet: {
-      url: "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
-      accounts: ["YOUR_PRIVATE_KEY"]
+      url: "https://mainnet.infura.io/v3/f158361d906a45c49c91c8b6cd197c2f",
+      accounts: ["TU_LLAVE_PRIVADA"]
     }
   },
   paths: {
